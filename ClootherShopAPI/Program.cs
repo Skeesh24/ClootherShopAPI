a0365cacf10f5532a52b9bf1e618e51ee19b0826
Skeesh24 <d.pacooloff@gmail.com>
2023-03-01 21:16:51 +0500

    Init

--- ClootherShopAPI/Program.cs
+++ ClootherShopAPI/Program.cs
@@ -1,8 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -19,7 +16,5 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
-
-app.MapRazorPages();
 
 app.Run();
